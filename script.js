aafcdb06cc079d480cfb4517aad5aebf71cf0637
Terry Babyuk <tbabyuk@gmail.com>
2022-12-24 03:16:27 -0500

    overhauled code using bootstrap CDN, improved layout and feel

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // DOM Elements
 
-const container = document.querySelector("#container");
-const inputNewItem = document.querySelector("#new_item_input");
-const formCreateItem = document.querySelector("#create_item_form");
+const table = document.querySelector("#table-items-container");
+const inputNewItem = document.querySelector("#new-item-input");
+const formCreateItem = document.querySelector("#create-item-form");
 const importanceLevels = document.getElementsByName("importance");
-const itemsColumn = document.querySelector("#items_column");
-const progressColumn = document.querySelector("#progress_column");
-const completedColumn = document.querySelector("#completed_column");
+const itemsColumn = document.querySelector("#items-column");
+const progressColumn = document.querySelector("#progress-column");
+const completedColumn = document.querySelector("#completed-column");
 
 
 
@@ -15,8 +15,12 @@ const completedColumn = document.querySelector("#completed_column");
 // create a new to-do item
 const createItem = (name, importance) => {
     const newItem = `
-        <li class="to-do-item ${importance}">${name}<i class="far fa-edit edit"></i><i class="far fa-trash-alt delete"></i><i class="fas fa-chevron-circle-right move"></i></li>
+        <li class="to-do-item ${importance} text-break border">
+            <span class="pe-3">${name}</span>
+            <span class="item-buttons d-flex justify-content-between align-items-center"><i class="far fa-edit edit"></i><i class="far fa-trash-alt delete"></i><i class="fas fa-chevron-circle-right move"></i></span>
+        </li>
     `;
+    console.log(newItem.children);
     itemsColumn.innerHTML += newItem;
     inputNewItem.value = "";
     inputNewItem.focus();
@@ -25,7 +29,7 @@ const createItem = (name, importance) => {
 
 //edit a todo item
 const editItem = (target, content) => {
-    target.innerHTML = content + '<i class="far fa-edit edit"></i><i class="far fa-trash-alt delete"></i><i class="fas fa-chevron-circle-right move"></i>';
+    target.innerText = content;
 };
 
 
@@ -37,7 +41,7 @@ const deleteItem = item => {
 
 // move a todo item
 const moveItem = item => {
-    if(item.parentElement.id === "items_column") {
+    if(item.parentElement.id === "items-column") {
         progressColumn.append(item);
     } else {
         completedColumn.append(item);
@@ -77,15 +81,14 @@ inputNewItem.addEventListener("focus", () => {
 
 
 // event bubbling on container, for editing, deleting, and moving to-do items
-container.addEventListener("click", e => {
+table.addEventListener("click", e => {
     if(e.target.className.includes("edit")) {
         let content = prompt("Enter new item name").toLowerCase();
-        editItem(e.target.parentElement, content);
+        editItem(e.target.parentElement.previousElementSibling, content);
     } else if(e.target.className.includes("delete")) {
-        deleteItem(e.target.parentElement);
+        deleteItem(e.target.parentElement.parentElement);
     } else if(e.target.className.includes("move")) {
-        moveItem(e.target.parentElement);
+        moveItem(e.target.parentElement.parentElement);
     }
 });
-
 
